c0c9efae7121bf93d259679fd40646fbe83005a4
gh-actions <actions@github.com>
2024-07-14 19:57:28 +0000

    Deploy website - based on 6657c1687f4e0b11cf9f33a68c33372509101ec5

---
+++ assets/files/port_scan-580e9022eab38385aaf27124d347a56e.ps1
@@ -0,0 +1,15 @@
+﻿param($hostname)
+
+$ports = 21,22,53,80,443,8080
+
+if ($hostname) {
+    ForEach ($port in $ports) {
+        $result = Test-NetConnection $hostname -Port $port -InformationLevel Quiet -WarningAction SilentlyContinue
+        if ($result) {
+            echo "TCP/$port Open"
+        }
+    }
+} else {
+    echo "Use: .\port_scan.ps1 <hostname>"
+    echo "E.g.: .\port_scan.ps1 8.8.8.8"
+}
