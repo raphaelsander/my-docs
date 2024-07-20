551626c9a25647bfd540800f1d5c1da5348dcbe2
gh-actions <actions@github.com>
2024-07-20 01:24:08 +0000

    Deploy website - based on 89aec637c1e19b6248949ebeb38ab212704a2a40

--- files/c/port_check.c
+++ files/c/port_check.c
@@ -1,15 +1,41 @@
 // gcc port_check.c -o port_check
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
-int main(void) {
+int is_invalid_ipv4(const char *ip) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+    return result == 0;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc <= 2){
+        printf("Use: %s <IP> <port>\n", argv[0]);
+        return 1;
+    }
+
     int sockfd;
     int connection;
-    int port = 80;
+    char *ip = argv[1];
+    char *endptr;
+    errno = 0;
+    long port = strtol(argv[2], &endptr, 10);
+
+    if (errno != 0 || *endptr != '\0' || port <= 0 || port > 65535) {
+        printf("port should be a number between 0 and 65535\n");
+        return 1;
+    }
+
+    if (is_invalid_ipv4(ip)) {
+        printf("invalid IP\n");
+        return 1;
+    }
 
     struct sockaddr_in target_addr;
 
@@ -19,19 +45,19 @@ int main(void) {
         perror("socket");
         return 1;
     }
-    
+
     target_addr.sin_family = AF_INET;
     target_addr.sin_port = htons(port);
-    target_addr.sin_addr.s_addr = inet_addr("192.168.0.1");
+    target_addr.sin_addr.s_addr = inet_addr(ip);
 
     connection = connect(sockfd, (struct sockaddr *)&target_addr, sizeof(target_addr));
 
-    if(connection == 0){
-        printf("port %i open\n", port);
+    if (connection == 0) {
+        printf("port %ld open\n", port);
         close(sockfd);
         close(connection);
     } else {
-        printf("port %i closed\n", port);
+        printf("port %ld closed\n", port);
     }
 
     return 0;
