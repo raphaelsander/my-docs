a8126e74df072f98990c40a04292a1405544c49c
Raphael Sander <raphael.sander75@gmail.com>
2026-02-14 17:08:47 -0300

    feat: upgrade docusaurus to v3.9.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,12 @@ const config = {
   projectName: 'my-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn'
+    }
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
